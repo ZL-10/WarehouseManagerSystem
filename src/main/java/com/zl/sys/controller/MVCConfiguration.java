1d8feaaeedd72a8a32331a7ac389e1f78c8c1c68
ZL-10 <1769377609@qq.com>
2020-02-14 17:53:45 +0800

    修复bug

---
+++ src/main/java/com/zl/sys/controller/MVCConfiguration.java
@@ -0,0 +1,20 @@
+package com.zl.sys.controller;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class MVCConfiguration implements WebMvcConfigurer {
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+
+        registry.addViewController("/").setViewName("forward:/index.html");
+
+        registry.setOrder(Ordered.HIGHEST_PRECEDENCE);
+
+
+    }
+}
